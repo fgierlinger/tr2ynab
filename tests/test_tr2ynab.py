b469cc1a2bf1e3d9e6a2c094767513dad0a4a494
fgierlinger <2966031+fgierlinger@users.noreply.github.com>
2025-11-23 17:41:01 +0100

    fix: enhance convert_value_string_to_milliunits for negative values and add tests

--- tests/test_tr2ynab.py
+++ tests/test_tr2ynab.py
@@ -6,3 +6,7 @@ def test_convert_value_string_to_milliunits():
     assert convert_value_string_to_milliunits("0.99") == 990
     assert convert_value_string_to_milliunits("100") == 100000
     assert convert_value_string_to_milliunits("12,345,678.901") == 12345678901
+    assert convert_value_string_to_milliunits("-40.28") == -40280
+    assert convert_value_string_to_milliunits("0") == 0
+    assert convert_value_string_to_milliunits("-0.01") == -10
+    assert convert_value_string_to_milliunits("-1,234.5") == -1234500
